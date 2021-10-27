da8c5039182588ec0103203c69e368b5c3e6fead
Link <cyd551646@sina.com>
2021-10-27 11:32:47 +0800

    feat: 定容数据结构完成, pop, 迭代器仍有问题

--- dist/数据结构/定容字符串栈/FixedCapacityStackOfStrings.js
+++ dist/数据结构/定容字符串栈/FixedCapacityStackOfStrings.js
@@ -2,16 +2,20 @@
 class FixedCapacityStackOfStrings {
     constructor(cap) {
         this.N = cap;
-        this.a = [];
+        this.a = new Array();
     }
     push(item) {
         if (this.isFull())
-            return;
+            this.resize(2 * this.a.length);
+        this.N++;
         this.a[this.a.length] = item;
     }
+    // 每次弹出元素数组都会递减, this.N 照理说是永远不会等于 this.a.length/4 ???
     pop() {
-        // if (this.N > 0 && this.N == this.a.length/4) resize(a.length/2);
-        return this.a.length--;
+        if (this.N > 0 && this.N == this.a.length / 4)
+            this.resize(this.a.length / 2);
+        this.N--;
+        return this.a.pop();
     }
     isEmpty() {
         return !this.a.length;
@@ -22,6 +26,14 @@ class FixedCapacityStackOfStrings {
     size() {
         return this.a.length;
     }
+    resize(max) {
+        let tamp = [];
+        this.a.forEach((item, index) => {
+            tamp[tamp.length] = item;
+        });
+        this.a = tamp;
+        this.N = max;
+    }
 }
 const F = new FixedCapacityStackOfStrings(5);
 console.log(F);
@@ -30,10 +42,4 @@ console.log(F.push('2'));
 console.log(F.push('3'));
 console.log(F.push('4'));
 console.log(F.push('5'));
-console.log(F.pop());
-console.log(F.pop());
-console.log(F.pop());
-console.log(F.push('6'));
-console.log(F.push('7'));
-console.log(F.pop());
 console.log(F);
--- 数据结构/定容字符串栈/FixedCapacityStackOfStrings.ts
+++ 数据结构/定容字符串栈/FixedCapacityStackOfStrings.ts
@@ -1,27 +1,44 @@
+
+
 // 一种表示定容字符串栈的抽象数据类型
 class FixedCapacityStackOfStrings<T> {
     private a: T[];
     private N: number;
     constructor(cap: number) {
         this.N = cap
-        this.a = []
+        this.a = new Array()
     }
-    push(item: T):void {
-        if(this.isFull()) return
+    public push(item: T):void {
+        if(this.isFull()) this.resize(2 * this.a.length)
+        this.N++
         this.a[this.a.length] = item
     }
-    pop():T {
-        // if (this.N > 0 && this.N == this.a.length/4) resize(a.length/2);
-        return this.a.length--
+    // 每次弹出元素数组都会递减, this.N 照理说是永远不会等于 this.a.length/4 ???
+    public pop():T {
+        if (this.N > 0 && this.N == this.a.length/4) this.resize(this.a.length/2);
+        this.N--
+        return this.a.pop()
     }
-    isEmpty(): boolean {
+
+    public isEmpty(): boolean {
         return !this.a.length
     }
-    isFull(): boolean {
+
+    public isFull(): boolean {
         return this.a.length >= this.N
     }
-    size(): number {
+
+    public size(): number {
         return this.a.length
+    }
+    
+    public resize(max: number):void {
+        let tamp: T[] = [];
+        this.a.forEach((item, index) => {
+            tamp[tamp.length] = item
+        })
+        this.a = tamp
+        this.N = max
     }
 }
 
@@ -32,10 +49,4 @@ console.log(F.push('2'));
 console.log(F.push('3'));
 console.log(F.push('4'));
 console.log(F.push('5'));
-console.log(F.pop());
-console.log(F.pop());
-console.log(F.pop());
-console.log(F.push('6'));
-console.log(F.push('7'));
-console.log(F.pop());
 console.log(F);
