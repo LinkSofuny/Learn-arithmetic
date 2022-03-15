8f45744b4a76601a2d1ad415d01769755833c830
Link <cyd551646@sina.com>
2022-03-15 17:50:20 +0800

    feat:  94. 二叉树的中序遍历.js

---
+++ leetCode系列/94. 二叉树的中序遍历.js
@@ -0,0 +1,21 @@
+
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {number[]}
+ */
+ var inorderTraversal = function(root) {
+    let res = []
+    if (!root) return []
+    res = res.concat(inorderTraversal(root.left))
+    res.push(root.val)
+    res = res.concat(inorderTraversal(root.right))
+    return res
+};
